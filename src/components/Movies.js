d48a570dd189848af51ec24aa887fbcfe146eaaf
abdallahsekrafi <abdalllah.sek@gmail.com>
2023-10-06 11:20:06 +0100

    project uploded

---
+++ src/components/Movies.js
@@ -0,0 +1,20 @@
+export const DefautlMovies = [
+    {
+        title: "The Creator",
+        description: "Décrit comme un thriller post-apocalyptique impliquant un futur marqué par une guerre entre les humains et l'IA",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BNDUyNTIzNDQtYTZmMi00M2FlLTgyZjUtYWViZWNhMDYzMjE4XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UY2048_.jpg",
+        rating: "4"
+    },
+    {
+        title: "No One Will Save You",
+        description: "Une femme au foyer, exilée et angoissée, doit lutter contre un extraterrestre qui s'est introduit dans sa maison",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BMGNhZDczNTUtOWEzZS00ZjEyLTkzODQtOGM1MTZiMGY2ODIyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_FMjpg_UY3000_.jpg",
+        rating: "3"
+    },
+    {
+        title: "Jumanji",
+        description: "Quatre adolescents sont aspirés dans un jeu vidéo magique, et la seule façon de s'échapper est de travailler ensemble",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BYzBmMTBmOWQtMTg3Zi00Mzk4LTk3OTMtYmU2NWMwYmJkNTU1XkEyXkFqcGdeQXVyMTEyOTQ1OTk4._V1_FMjpg_UX1200_.jpg",
+        rating: "5"
+    },
+];
